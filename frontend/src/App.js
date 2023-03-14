0007580e74369c937e39a3392c97cdee29764f2e
Shashwat <shashwatrauthan@gmail.com>
2023-03-14 17:53:45 +0530

    shashwat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Landing from './pages/Landing';
